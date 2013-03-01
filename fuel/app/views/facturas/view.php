ca7f110d2cf60802fc7e40241e2f8d03cb751b65
ronaldpatino <ronaldpatino@gmail.com>
2013-03-01 16:32:57 -0500

    Agregado tooltip para mostraar comentarios ingresados

--- fuel/app/views/facturas/view.php
+++ fuel/app/views/facturas/view.php
@@ -7,19 +7,25 @@
         <th>Fecha</th>
         <th>Tipo</th>
         <th>Valor</th>
-        <th></th>
     </tr>
     </thead>
     <tbody>
-        <?php foreach ($facturas as $factura): ?>		<tr>
+        <?php foreach ($facturas as $factura): ?>
+    <tr>
 
 
-        <td><?php echo $factura->fecha; ?></td>
-        <td><?php echo $tipos_deducibles[$factura->tipo]; ?></td>
-        <td><?php echo $factura->valor; ?></td>
         <td>
-            <?php echo Html::anchor('facturas/view/'.$factura->ruc, 'Detalle'); ?>
+            <?php if (!empty($factura->comentario)):?>
+            <a class="btn btn-primary btn-mini comentario" href="#" data-toggle='tooltip' title='<?php echo htmlspecialchars($factura->comentario, ENT_QUOTES);?>'><i class="icon-tag icon-white"></i></a>
+            <?php else:?>
+            <a class="btn btn-primary btn-mini disabled" href="#" data-toggle='tooltip' title='<?php echo htmlspecialchars($factura->comentario, ENT_QUOTES);?>'><i class="icon-tag icon-white"></i></a>
+            <?php endif;?>
+            <?php echo $factura->fecha;?>
         </td>
+
+        <td><?php echo  $tipos_deducibles[$factura->tipo]; ?></td>
+        <td>$ <?php echo $factura->valor; ?></td>
+
     </tr>
         <?php endforeach; ?>	</tbody>
 </table>
@@ -28,9 +34,18 @@
 <?php else: ?>
 <p>No Facturas.</p>
 
-<?php endif; ?><p>
-    <?php echo Html::anchor('facturas/create', 'Add new Factura', array('class' => 'btn btn-success')); ?>
+<?php endif; ?>
+<div class="btn-group">
+    <?php echo Html::anchor('facturas/create', 'Registrar Nueva Factura', array('class' => 'btn btn-success')); ?>
+    <?php echo Html::anchor('facturas', 'Ir al listado de facturas', array('class' => 'btn')); ?>
+</div>
 
-</p>
 
 
+
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.comentario').tooltip({placement:'right',trigger:'hover'});
+    });
+</script>

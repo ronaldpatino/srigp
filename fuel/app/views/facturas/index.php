35804f437b428cd058e22d2750e0640c9a811832
ronaldpatino <ronaldpatino@gmail.com>
2013-03-01 16:30:07 -0500

    Agregada paginacion

--- fuel/app/views/facturas/index.php
+++ fuel/app/views/facturas/index.php
@@ -17,18 +17,18 @@
 			<td><?php echo $factura->ruc; ?></td>
 			<td><?php echo Str::upper($factura->nombre); ?></td>
 
-			<td><?php echo $factura->total; ?></td>
+			<td>$ <?php echo $factura->total; ?></td>
 			<td>
 				<?php echo Html::anchor('facturas/view/'.$factura->ruc, 'Detalle'); ?>
 			</td>
 		</tr>
 <?php endforeach; ?>	</tbody>
 </table>
-
+<?php echo $pagination->render(); ?>
 <?php else: ?>
 <p>No Facturas.</p>
 
 <?php endif; ?><p>
-	<?php echo Html::anchor('facturas/create', 'Add new Factura', array('class' => 'btn btn-success')); ?>
+	<?php echo Html::anchor('facturas/create', 'Registrar Nueva Factura', array('class' => 'btn btn-success')); ?>
 
 </p>

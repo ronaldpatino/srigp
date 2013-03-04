c7c62392b33d047a62404bc90502d04b1e56029b
ronaldpatino <ronaldpatino@gmail.com>
2013-03-04 12:06:34 -0500

    Evaluando raphael y g.raphael para graficos

--- fuel/app/classes/controller/welcome.php
+++ fuel/app/classes/controller/welcome.php
@@ -19,7 +19,7 @@
  * @package  app
  * @extends  Controller
  */
-class Controller_Welcome extends Controller
+class Controller_Welcome extends Controller_Template
 {
 
 	/**
@@ -30,7 +30,96 @@ class Controller_Welcome extends Controller
 	 */
 	public function action_index()
 	{
-		return Response::forge(View::forge('welcome/index'));
+
+        $query = DB::query('SELECT
+                                    ruc,
+                                    nombre ,
+                                    tipo,
+                                    ( SELECT
+                                            SUM(valor)
+                                       FROM
+                                            facturas si
+                                       WHERE
+                                            si.ruc like so.ruc)
+                                    total
+                            FROM
+                                facturas so
+                            GROUP BY ruc
+
+                            ORDER BY nombre  ASC, ruc ASC')->as_object('Model_Factura')->execute();
+
+        $datos = array();
+        $label = array();
+        foreach($query as $r)
+        {
+            array_push($datos, $r->total);
+            array_push($label, $r->nombre);
+        }
+        $data['datos'] = '[' . implode(',',$datos) . ']';
+        $data['label'] = "['%%.%% - " . implode("','%%.%% - ",$label) . "']";
+
+
+        /*****************************************************************************/
+
+
+        $query = DB::query('SELECT
+                                    ( SELECT
+                                            SUM(valor)
+                                       FROM
+                                            facturas si
+                                       WHERE
+                                            si.tipo = 1)
+                                    tipo1,
+                                    ( SELECT
+                                            SUM(valor)
+                                       FROM
+                                            facturas si
+                                       WHERE
+                                            si.tipo = 2)
+                                    tipo2,
+                                    ( SELECT
+                                            SUM(valor)
+                                       FROM
+                                            facturas si
+                                       WHERE
+                                            si.tipo = 3)
+                                    tipo3,
+                                    ( SELECT
+                                            SUM(valor)
+                                       FROM
+                                            facturas si
+                                       WHERE
+                                            si.tipo = 4)
+                                    tipo4,
+                                    ( SELECT
+                                            SUM(valor)
+                                       FROM
+                                            facturas si
+                                       WHERE
+                                            si.tipo = 5)
+                                    tipo5
+                            FROM
+                                facturas so
+                            GROUP BY tipo')->as_object('Model_Factura')->execute();
+
+        $datos = array();
+
+
+
+            array_push($datos, ($query[0]->tipo1 >0)?$query[0]->tipo1:0);
+            array_push($datos, ($query[0]->tipo2 >0)?$query[0]->tipo2:0);
+            array_push($datos, ($query[0]->tipo3 >0)?$query[0]->tipo3:0);
+            array_push($datos, ($query[0]->tipo4 >0)?$query[0]->tipo4:0);
+            array_push($datos, ($query[0]->tipo5 >0)?$query[0]->tipo51:0);
+
+
+
+        $data['datos_categoria'] = '[' . implode(',',$datos) . ']';
+        $data['label_categoria'] = "['%%.%% - uno','%%.%% - dos','%%.%% - tres','%%.%% - cuatro','%%.%% - cinco']";
+
+        $view = View::forge('welcome/index', $data);
+        $this->template->title = "Facturas";
+        $this->template->content = $view;
 	}
 
 	/**

f973442d330f2334c536ae20e8c06a36ff159d90
ronaldpatino <ronaldpatino@gmail.com>
2013-03-07 10:41:40 -0500

    Reportes tiene su propio controlador ahora

---
+++ fuel/app/classes/controller/reportes.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.5
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+/**
+ * The Welcome Controller.
+ *
+ * A basic controller example.  Has examples of how to set the
+ * response body and status.
+ *
+ * @package  app
+ * @extends  Controller
+ */
+class Controller_Reportes extends Controller_Template
+{
+
+	/**
+	 * The basic reportes message
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_index()
+	{
+
+        $query = DB::query('SELECT
+                                    ruc,
+                                    nombre ,
+                                    tipo,
+                                    ( SELECT
+                                            SUM(valor)
+                                       FROM
+                                            facturas si
+                                       WHERE
+                                            si.ruc like so.ruc)
+                                    total
+                            FROM
+                                facturas so
+                            GROUP BY ruc
+
+                            ORDER BY nombre  ASC, ruc ASC')->as_object('Model_Factura')->execute();
+
+        $datos = array();
+        $label = array();
+        foreach($query as $r)
+        {
+            array_push($datos, $r->total);
+            array_push($label, $r->nombre);
+        }
+        $data['datos'] = '[' . implode(',',$datos) . ']';
+        $data['label'] = "['%%.%% - " . implode("','%%.%% - ",$label) . "']";
+
+
+        /*****************************************************************************/
+        $tipos_deducibles = $this->get_categorias();
+
+        $tda = array();
+        foreach($tipos_deducibles as $i => $td)
+        {
+
+            $cadena = "( SELECT SUM(valor) FROM facturas si WHERE si.tipo = {$i}) tipo{$i}";
+            array_push($tda, $cadena);
+
+        }
+
+        $subsql = implode(',',$tda);
+
+        $query = DB::query('SELECT ' .
+                                    $subsql .
+                            ' FROM
+                                facturas so
+                            GROUP BY tipo')->execute();
+
+        $datos = array();
+        $label_categoria = array();
+
+        foreach($tipos_deducibles as $i => $td)
+        {
+            $indice = 'tipo' . $i;
+            array_push($datos, ($query[0][$indice] >0)?$query[0][$indice]:0);
+            array_push($label_categoria, $td);
+        }
+
+        $data['datos_categoria'] = '[' . implode(',',$datos) . ']';
+        $data['label_categoria'] = $this->utf8_urldecode(  "['%%.%% - " . implode("','%%.%% - ",$label_categoria) . "']");
+
+
+        $view = View::forge('reportes/index', $data);
+        $view->set_global('reportes', '1');
+        $this->template->title = "Facturas";
+        $this->template->content = $view;
+	}
+
+	/**
+	 * A typical "Hello, Bob!" type example.  This uses a ViewModel to
+	 * show how to use them.
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_hello()
+	{
+		return Response::forge(ViewModel::forge('reportes/hello'));
+	}
+
+	/**
+	 * The 404 action for the application.
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_404()
+	{
+		return Response::forge(ViewModel::forge('reportes/404'), 404);
+	}
+
+    private function get_categorias()
+    {
+        $categorias = Model_Categoria::find('all');
+        foreach($categorias as $c)
+        {
+            $resultado[$c->id] = $c->nombre;
+        }
+
+        return isset($resultado)?$resultado:0;
+
+    }
+
+    private function utf8_urldecode($str) {
+        $str = preg_replace("/%u([0-9a-f]{3,4})/i","&#x\\1;",urldecode($str));
+        return html_entity_decode($str,null,'UTF-8');;
+    }
+}
+
---
+++ fuel/app/views/reportes/index.php
@@ -0,0 +1,68 @@
+<style type="text/css">
+
+    #gastos_proveedor_container{
+        -moz-border-radius: 10px;
+        -webkit-border-radius: 10px;
+        -webkit-box-shadow: 0 1px 3px #666;
+        width: 580px;
+        height: 370px;
+        margin :1em;
+        background:url(../assets/img/bg.png) right bottom no-repeat
+    }
+
+    #gastos_categoria_container{
+        -moz-border-radius: 10px;
+        -webkit-border-radius: 10px;
+        -webkit-box-shadow: 0 1px 3px #666;
+        width: 580px;
+        height: 370px;
+
+        margin :1em;
+        background:url(../assets/img/bg.png) right bottom no-repeat
+    }
+
+</style>
+<div id="gastos_proveedor_container"></div>
+<div id="gastos_categoria_container"></div>
+
+<script>
+
+    var gastos_proveedor = new Object();
+    // hero graph
+    gastos_proveedor.container = "gastos_proveedor_container";
+    gastos_proveedor.pieLegendPos = "east";
+    gastos_proveedor.legendTop = 70;
+    gastos_proveedor.legendLeft = 340;
+    gastos_proveedor.pieRadius = 130;
+    gastos_proveedor.pieXpos = 150;
+    gastos_proveedor.pieYpos = 180;
+    gastos_proveedor.pieData = <?php echo $datos?>;
+    gastos_proveedor.pieLegend = <?php echo $label?>;
+    gastos_proveedor.legend = "Gastos por Proveedor";
+
+
+    var gastos_categoria = new Object();
+    // hero graph
+    gastos_categoria.container = "gastos_categoria_container";
+    gastos_categoria.pieLegendPos = "east";
+    gastos_categoria.legendTop = 70;
+    gastos_categoria.legendLeft = 340;
+    gastos_categoria.pieRadius = 130;
+    gastos_categoria.pieXpos = 150;
+    gastos_categoria.pieYpos = 180;
+    gastos_categoria.pieData = <?php echo $datos_categoria?>;
+    gastos_categoria.pieLegend = <?php echo $label_categoria?>;
+    gastos_categoria.legend = "Gastos por Categoría";
+
+    $(document).ready(function(){
+
+        if(gastos_proveedor.hasOwnProperty('container')) {
+            setGraph(gastos_proveedor);
+        }
+
+        if(gastos_categoria.hasOwnProperty('container')) {
+            setGraph(gastos_categoria);
+        }
+    });
+
+</script>
